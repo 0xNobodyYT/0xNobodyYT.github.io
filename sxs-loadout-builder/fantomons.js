e26292486e5d222c4d0b6e7147dac1120339aa9c
0xNobodyYT <athanasioujohn@gmail.com>
2026-08-30 19:36:31 +0300

    Refine loadout builder and correct dungeon names

---
+++ sxs-loadout-builder/fantomons.js
@@ -0,0 +1,18 @@
+window.FANTOMON_CATALOG=[
+ {id:1600,name:"Falko",type:"Attacker"},
+ {id:1601,name:"Boaro",type:"Tank"},
+ {id:1602,name:"Mandragora",type:"Support"},
+ {id:1603,name:"Zeioletus",type:"Attacker"},
+ {id:1604,name:"Terragon",type:"Tank"},
+ {id:1605,name:"Sylvaerie",type:"Support"},
+ {id:1606,name:"Kels",type:"Attacker"},
+ {id:1607,name:"Armopi",type:"Tank"},
+ {id:1608,name:"Herbote",type:"Support"},
+ {id:1609,name:"Nyxarchon",type:"Attacker"},
+ {id:1610,name:"Aegiswing",type:"Tank"},
+ {id:1611,name:"Pandarial",type:"Support"},
+ {id:1612,name:"Chomusuke",type:"Support"},
+ {id:1613,name:"Cabbage Dog",type:"Tank"},
+ {id:1614,name:"Prismora",type:"Support"},
+ {id:1615,name:"Luminarch Steed",type:"Tank"}
+];
